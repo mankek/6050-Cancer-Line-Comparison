baf89f738325f21e1e18a44aaf7deba3563fb99a
Krystal Manke <krystal.manke@gmail.com>
2019-09-22 18:50:36 -0500

    User interface has button to open file explorer

--- App.py
+++ App.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+from tkinter import *
+import os
+
+
+class App:
+
+    def __init__(self, master):
+
+        frame_1 = Frame(master)
+        frame_1.pack()
+        w = Label(frame_1, text="Welcome to our application!")
+        w.pack()
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+            )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Open File Explorer", command=self.open_files)
+        self.hi_there.pack(side=LEFT)
+
+    @staticmethod
+    def open_files():
+        os.system("explorer C:\\")
+
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
