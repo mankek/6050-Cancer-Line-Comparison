4dced3a5e495df28a6e03ca2b4fae1b2e1e2b9b7
Krystal Manke <krystal.manke@gmail.com>
2019-09-22 22:29:44 -0500

    allows choosing of files and makes window for analysis

--- App.py
+++ App.py
@@ -1,34 +1,62 @@
 from tkinter import *
+from tkinter import filedialog
 import os
 
 
 class App:
 
     def __init__(self, master):
+        self.master = master
+        self.chosen_files = []
 
         frame_1 = Frame(master)
         frame_1.pack()
         w = Label(frame_1, text="Welcome to our application!")
         w.pack()
 
-        frame = Frame(master)
-        frame.pack()
+        button_frame = Frame(master)
+        button_frame.pack()
 
-        self.button = Button(
-            frame, text="QUIT", fg="red", command=frame.quit
+        self.quit_button = Button(
+            button_frame, text="QUIT", fg="red", command=button_frame.quit
             )
-        self.button.pack(side=LEFT)
+        self.quit_button.pack(side=LEFT)
 
-        self.hi_there = Button(frame, text="Open File Explorer", command=self.open_files)
-        self.hi_there.pack(side=LEFT)
+        self.file_button = Button(button_frame, text="Open File Explorer", command=self.open_files)
+        self.file_button.pack(side=LEFT)
 
-    @staticmethod
-    def open_files():
-        os.system("explorer C:\\")
+        self.analyze_button = Button(button_frame, text="Analyze files", command=self.analyze)
+        self.analyze_button.pack(side=LEFT)
+
+        self.frame_2 = Frame(master)
+        self.frame_2.pack()
+
+    def open_files(self):
+        filename = filedialog.askopenfilename(initialdir="C:\\", title="Select File")
+        self.chosen_files.append(filename)
+        v = Label(self.frame_2, text=filename)
+        v.pack()
+
+    def analyze(self):
+        root_2 = Tk()
+        app_2 = Analyze(self.chosen_files, root_2)
+        root_2.mainloop()
+
+
+class Analyze:
+
+    def __init__(self, file_list, master):
+        frame_1 = Frame(master)
+        frame_1.pack()
+        w = Label(frame_1, text="Welcome to our application!")
+        w.pack()
+
+        frame_2 = Frame(master)
+        frame_2.pack()
+        a = Label(frame_2, text=str(len(file_list)) + " files chosen!")
+        a.pack()
 
 
 root = Tk()
-
 app = App(root)
-
 root.mainloop()
