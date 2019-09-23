1a798cbe89b8cfef1147cc4865c770cfde794d4c
Krystal Manke <krystal.manke@gmail.com>
2019-09-23 18:29:50 -0500

    refined file list and added scrollbar

--- App.py
+++ App.py
@@ -11,7 +11,7 @@ class App:
 
         frame_1 = Frame(master)
         frame_1.pack()
-        w = Label(frame_1, text="Welcome to our application!")
+        w = Label(frame_1, text="Welcome to our application!", pady=5)
         w.pack()
 
         button_frame = Frame(master)
@@ -28,14 +28,20 @@ class App:
         self.analyze_button = Button(button_frame, text="Analyze files", command=self.analyze)
         self.analyze_button.pack(side=LEFT)
 
-        self.frame_2 = Frame(master)
+        self.frame_2 = Frame(master, bd=10)
         self.frame_2.pack()
+        scrollbar = Scrollbar(self.frame_2, orient=HORIZONTAL)
+        scrollbar.pack(side=BOTTOM, fill=X)
+        frame_2_title = Label(self.frame_2, text="Selected Files", pady=5)
+        frame_2_title.pack()
+        self.listbox = Listbox(self.frame_2, width=30, height=5, xscrollcommand=scrollbar.set)
+        self.listbox.pack()
+        scrollbar.config(command=self.listbox.xview)
 
     def open_files(self):
         filename = filedialog.askopenfilename(initialdir="C:\\", title="Select File")
         self.chosen_files.append(filename)
-        v = Label(self.frame_2, text=filename)
-        v.pack()
+        self.listbox.insert(END, filename)
 
     def analyze(self):
         root_2 = Tk()
