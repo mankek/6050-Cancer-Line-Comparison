5fffcb43ef2557bb4809b82baefc4c231cdf18ab
Krystal Manke <krystal.manke@gmail.com>
2019-10-06 21:41:13 -0500

    added CCLE tissue selection to interface

--- Interface.py
+++ Interface.py
@@ -6,85 +6,186 @@ from tkinter import *
 # from tkinter import filedialog
 import os
 
+file_dir = "\\".join(os.path.dirname(os.path.abspath(__file__)).split("\\")[0:]) + r"\Data-Files"
+
 
 class Interface:
 
     def __init__(self, master):
-        self.master = master
-        self.organ_list = ["Kidney", "Lip", "Ovary", "Skin", "Bladder", "Blood"]
-        self.selected_organ = ""
+        self.top_frame = Frame(master)
+        self.top_frame.pack()
+        self.middle_frame = Frame(master, bd=10)
+        self.middle_frame.pack()
+        self.bottom_frame = Frame(master, bd=10)
+        self.bottom_frame.pack()
+        # self.organ_list = ["Kidney", "Lip", "Ovary", "Skin", "Bladder", "Blood"]
+        self.gdc_organ_list = self.get_GDC_organs()
+        self.ccle_organ_list = self.get_CCLE_organs()
+        self.selected_gdc_organ = None
+        self.selected_ccle_organ = None
         self.title_frame()
-        self.button_menu()
-        self.organ_menu()
+        self.GDC_frame()
+        self.CCLE_frame()
         self.notifications()
 
+    def get_GDC_organs(self):
+        with open(r"GDC Tissue Types.tsv", "r") as organ_file:
+            for i in organ_file.readlines():
+                organ_list = i.rstrip().split("\t")
+        return organ_list
+
+    def get_CCLE_organs(self):
+        with open(r"CCLE Tissue Types.csv", "r") as organ_file:
+            organ_list = []
+            for i in organ_file.readlines()[1:]:
+                organ_list.append(i.split(",")[0])
+            return organ_list
+
     def title_frame(self):
-        frame_1 = Frame(self.master)
+        frame_1 = Frame(self.top_frame)
         frame_1.pack()
         w = Label(frame_1, text="Welcome to our application!", pady=5)
         w.pack()
-
-    def button_menu(self):
-        button_frame = Frame(self.master)
+        button_frame = Frame(frame_1)
         button_frame.pack()
-        quit_button = Button(
+        self.quit_button = Button(
             button_frame, text="QUIT", fg="red", command=button_frame.quit
-            )
-        quit_button.pack(side=LEFT)
-        query_button = Button(button_frame, text="Query files", command=self.query)
-        query_button.pack(side=LEFT)
-        analyze_button = Button(button_frame, text="Analyze files", command=self.analyze)
-        analyze_button.pack(side=LEFT)
+        )
+        self.quit_button.pack(side=LEFT)
+        self.analyze_button = Button(button_frame, text="Analyze files", command=self.analyze)
+        self.analyze_button.pack(side=LEFT)
 
-    def organ_menu(self):
-        frame_2 = Frame(self.master, bd=10)
-        frame_2.pack()
-        scrollbar = Scrollbar(frame_2, orient=VERTICAL)
+    def GDC_frame(self):
+        gdc_frame = Frame(self.middle_frame)
+        gdc_frame.pack(side=LEFT)
+        gdc_title = Label(gdc_frame, text="GDC Menu", pady=5)
+        gdc_title.pack()
+        gdc_buttons = Frame(gdc_frame)
+        gdc_buttons.pack()
+        query_button = Button(gdc_buttons, text="Query Tissue", command=self.query)
+        query_button.pack(side=LEFT)
+        gdc_organs = Frame(gdc_frame, bd=10)
+        gdc_organs.pack()
+        scrollbar = Scrollbar(gdc_organs, orient=VERTICAL)
         scrollbar.pack(side=RIGHT, fill=Y)
-        frame_2_title = Label(frame_2, text="Organ Options", pady=5)
-        frame_2_title.pack()
-        self.listbox = Listbox(frame_2, height=5, yscrollcommand=scrollbar.set, selectmode=SINGLE)
-        self.listbox.pack()
-        scrollbar.config(command=self.listbox.yview)
-        for organ in self.organ_list:
-            self.listbox.insert(END, organ)
-        # self.listbox.bind("<Button-1>", self.notify_organ_selection)
+        organ_menu_title = Label(gdc_organs, text="Tissue Options", pady=5)
+        organ_menu_title.pack()
+        self.gdc_listbox = Listbox(gdc_organs, width=40, height=5, yscrollcommand=scrollbar.set, selectmode=SINGLE)
+        self.gdc_listbox.pack()
+        scrollbar.config(command=self.gdc_listbox.yview)
+        for organ in self.gdc_organ_list:
+            self.gdc_listbox.insert(END, organ)
+
+    def CCLE_frame(self):
+        ccle_frame = Frame(self.middle_frame)
+        ccle_frame.pack(side=LEFT)
+        ccle_title = Label(ccle_frame, text="CCLE Menu", pady=5)
+        ccle_title.pack()
+        ccle_buttons = Frame(ccle_frame)
+        ccle_buttons.pack()
+        select_button = Button(ccle_buttons, text="Select Tissue", command=self.selection)
+        select_button.pack(side=LEFT)
+        ccle_organs = Frame(ccle_frame, bd=10)
+        ccle_organs.pack()
+        scrollbar = Scrollbar(ccle_organs, orient=VERTICAL)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        organ_menu_title = Label(ccle_organs, text="Tissue Options", pady=5)
+        organ_menu_title.pack()
+        self.ccle_listbox = Listbox(ccle_organs, width=40, height=5, yscrollcommand=scrollbar.set, selectmode=SINGLE)
+        self.ccle_listbox.pack()
+        scrollbar.config(command=self.ccle_listbox.yview)
+        for organ in self.ccle_organ_list:
+            self.ccle_listbox.insert(END, organ)
 
     def notifications(self):
-        frame_3 = Frame(self.master, bd=10)
-        frame_3.pack()
-        frame_3_title = Label(frame_3, text="Notifications", pady=5)
-        frame_3_title.pack()
-        self.text_box = Text(frame_3, height=5, width=30)
+        note_frame = Frame(self.bottom_frame, bd=5, relief=SUNKEN)
+        note_frame.pack(side=BOTTOM)
+        scrollbar = Scrollbar(note_frame, orient=VERTICAL)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        note_title = Label(note_frame, text="Notifications", pady=5)
+        note_title.pack()
+        self.text_box = Text(note_frame, height=5, width=50, state=DISABLED, yscrollcommand=scrollbar.set)
         self.text_box.pack()
-
-    # def notify_organ_selection(self, event):
-    #     clicked_ind = self.listbox.curselection()
-    #     print(clicked_ind)
-        # self.selected_organ = str(self.organ_list[clicked_ind])
+        scrollbar.config(command=self.text_box.yview)
 
     def query(self):
-        clicked_ind = self.listbox.curselection()[0]
-        self.selected_organ = str(self.organ_list[clicked_ind])
-        query_obj = GDCQuery(self.selected_organ)
-        self.text_box.insert(INSERT, self.selected_organ + " was selected and query has begun!\n")
-        resp, files = query_obj.query_files()
-        self.text_box.insert(INSERT, "Response: " + str(resp) + "\nFiles found: " + str(len(files)))
-        # query_obj.query_data(5)
+        if self.gdc_listbox.curselection():
+            clicked_ind = self.gdc_listbox.curselection()[0]
+            self.selected_gdc_organ = str(self.gdc_organ_list[clicked_ind])
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, self.selected_gdc_organ + " was selected!\n")
+            self.text_box.config(state="disabled")
+            self.query_obj = GDCQuery(self.selected_gdc_organ)
+            resp, files = self.query_obj.query_files()
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, "Response: " + str(resp) + "\nFiles found: " + str(len(files)) + "\n")
+            self.text_box.insert(INSERT, "Querying data...\n")
+            self.text_box.config(state="disabled")
+            result_text = self.query_obj.query_data(5)
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, result_text + "\n")
+            self.text_box.config(state="disabled")
+        else:
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, "No GDC tissue is selected.\n")
+            self.text_box.config(state="disabled")
+
+    def selection(self):
+        if self.ccle_listbox.curselection():
+            clicked_ind = self.ccle_listbox.curselection()[0]
+            self.selected_ccle_organ = str(self.ccle_organ_list[clicked_ind])
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, self.selected_ccle_organ + " was selected!\n")
+            self.text_box.config(state="disabled")
+        else:
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, "No CCLE tissue is selected.\n")
+            self.text_box.config(state="disabled")
 
     def analyze(self):
-        root_2 = Tk()
-        app_2 = Analyze(root_2)
-        root_2.mainloop()
+        if self.selected_gdc_organ and self.selected_ccle_organ:
+            # self.query_button.config(state="disabled")
+            self.analyze_button.config(state="disabled")
+            root_2 = Tk()
+            app_2 = Analyze(root_2, self.query_obj)
+            root_2.mainloop()
+        elif not self.selected_gdc_organ:
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, "No GDC tissue is selected.\n")
+            self.text_box.config(state="disabled")
+        elif not self.selected_ccle_organ:
+            self.text_box.config(state="normal")
+            self.text_box.insert(INSERT, "No CCLE tissue is selected.\n")
+            self.text_box.config(state="disabled")
 
 
 class Analyze:
 
-    def __init__(self, master):
-        frame_1 = Frame(master)
-        frame_1.pack()
-        w = Label(frame_1, text="Welcome to our application!")
+    def __init__(self, master, query_obj):
+        self.master = master
+        self.query_object = query_obj
+        self.files_left = len(query_obj.file_uuid_list)
+        self.title_frame()
+        self.file_frame()
+
+    def title_frame(self):
+        self.frame_1 = Frame(self.master)
+        self.frame_1.pack()
+        w = Label(self.frame_1, text="Welcome to our application!")
         w.pack()
+
+    def file_frame(self):
+        z = Label(self.frame_1, text=str(self.files_left) + " patient files left!", pady=5)
+        z.pack()
+        frame_2 = Frame(self.master)
+        frame_2.pack()
+        y = Label(frame_2, text="Patient data currently available:")
+        y.pack()
+        for patient in self.query_object.data_list:
+            patient_id = patient.keys()
+            y = Label(self.frame_1, text=str(patient_id), pady=5)
+            y.pack()
+
 
 class GDCQuery:
 
@@ -93,6 +194,7 @@ class GDCQuery:
         self.organ = organ
         self.data_endpt = "https://api.gdc.cancer.gov/data"
         self.file_uuid_list = []
+        self.data_list = list()
 
     def param_construct(self):
         filters = {
@@ -109,7 +211,7 @@ class GDCQuery:
                 "op": "in",
                 "content":{
                     "field": "files.analysis.workflow_type",
-                    "value": ["HTSeq - Counts", "STAR - Counts"]
+                    "value": ["HTSeq - Counts"]
                     }
                 },
                 {
@@ -148,27 +250,31 @@ class GDCQuery:
 
     def query_data(self, num_patients):
         sub_file_ids = self.file_uuid_list[:num_patients]
-        for file_id in sub_file_ids:
+        for file_index, file_id in enumerate(sub_file_ids):
+            self.file_uuid_list.remove(file_id)
             params = {"ids": file_id}
             response = requests.post(self.data_endpt, data=json.dumps(params), headers={"Content-Type": "application/json"})
             self.parse_data(response)
+        return "Data from " + str(num_patients) + " files has been parsed!\n"
 
     def parse_data(self, response):
         response_head_cd = response.headers["Content-Disposition"]
         file_name = re.findall("filename=(.+)", response_head_cd)[0]
-        file_path = os.path.join(r"6050-Cancer-Line-Comparison/Data-Files", file_name)
+        data_dict = dict()
+        data_dict[file_name] = {}
+        file_path = os.path.join(file_dir, file_name)
         with open(file_path, "wb") as output_file:
             output_file.write(response.content)
 
         with gzip.open(file_path, "rb") as gene_exp:
-            for i in gene_exp.readlines()[:5]:
-                print(i.decode("utf8"))
-            print("\n")
+            for i in gene_exp.readlines():
+                i_split = i.decode().split("\t")
+                gene_id = i_split[0]
+                gene_count = i_split[1]
+                data_dict[file_name][gene_id] = gene_count
+        self.data_list.append(data_dict)
 
 
 root = Tk()
 app = Interface(root)
 root.mainloop()
-
-
-
